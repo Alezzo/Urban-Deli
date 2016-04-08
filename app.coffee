69c2673f8a7472c669d9c2eb7cdd3018ab013e9e
Aleksandar <aleks.zezovski@gmail.com>
2016-04-08 11:02:13 +0200

    Start -> Home transition working

--- app.coffee
+++ app.coffee
@@ -6,29 +6,18 @@ vnc = new ViewNavigationController
 vnc.animationOptions =
 	curve: "ease-in-out"
 	time: 0.3
-	
+
 # Import file "Start screen"
 psd = Framer.Importer.load("imported/Start screen@1x")
 
-#This is the initial view 
-psd = new Layer
-	name: "initialView"
-	width: Screen.width
-	height: Screen.height
-	superLayer: vnc
+psd.startScreen.name = "initialView"
+psd.startScreen.superLayer = vnc
 
-#This is the second view that should be accessed when pressing Svevägen
 homeView = new Layer
 	width: Screen.width
 	height: Screen.height
 	backgroundColor: "#FCFAED"
 	superLayer: vnc
-
-btnHome = new Layer
-	width: Screen.width
-	height: Screen.height/3
-	backgroundColor: "transparent"
-	superLayer: psd
 	
-btnHome.on Events.Click, ->
+psd.sveaButton.on Events.Click, ->
 	vnc.transition homeView
--- imported/Start screen@1x/layers.json.js
+++ imported/Start screen@1x/layers.json.js
@@ -1,8 +1,8 @@
 window.__imported__ = window.__imported__ || {};
 window.__imported__["Start screen@1x/layers.json.js"] = [
 	{
-		"id": 10,
-		"name": "Grupp_1",
+		"id": 20,
+		"name": "startScreen",
 		"layerFrame": {
 			"x": 0,
 			"y": 0,
@@ -10,19 +10,85 @@ window.__imported__["Start screen@1x/layers.json.js"] = [
 			"height": 1334
 		},
 		"maskFrame": null,
-		"image": {
-			"path": "images/Grupp_1.png",
-			"frame": {
-				"x": 0,
-				"y": 0,
-				"width": 750,
-				"height": 1332
-			}
-		},
-		"imageType": "png",
+		"image": null,
+		"imageType": null,
 		"children": [
-			
+			{
+				"id": 18,
+				"name": "sicklaButton",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 750,
+					"height": 1334
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/sicklaButton.png",
+					"frame": {
+						"x": 0,
+						"y": 888,
+						"width": 750,
+						"height": 444
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "226778623"
+			},
+			{
+				"id": 16,
+				"name": "nytorgetButton",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 750,
+					"height": 1334
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/nytorgetButton.png",
+					"frame": {
+						"x": 0,
+						"y": 444,
+						"width": 750,
+						"height": 444
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "226778623"
+			},
+			{
+				"id": 14,
+				"name": "sveaButton",
+				"layerFrame": {
+					"x": 0,
+					"y": 0,
+					"width": 750,
+					"height": 1334
+				},
+				"maskFrame": null,
+				"image": {
+					"path": "images/sveaButton.png",
+					"frame": {
+						"x": 0,
+						"y": 0,
+						"width": 750,
+						"height": 444
+					}
+				},
+				"imageType": "png",
+				"children": [
+					
+				],
+				"modification": "226778624"
+			}
 		],
-		"modification": "559382427"
+		"modification": "1664346582"
 	}
 ]
